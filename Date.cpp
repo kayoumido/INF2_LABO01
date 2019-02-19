4f982ce8a025d86134c83f6df18de9ad6b7737e5
Doran Kayoumi <dorankayoumi@gmail.com>
2019-02-19 17:39:47 +0100

    Add constructor

--- Date.cpp
+++ Date.cpp
@@ -3,3 +3,27 @@
  */
 
 #include "Date.h"
+
+Date::Date(unsigned d, unsigned m, unsigned y) {
+    day = d;
+    month = m;
+    year = y;
+
+    correct = isCorrect();
+}
+
+Date::Date(unsigned d, Month m, unsigned y) {
+    day = d;
+    setMonth(m);
+    year = y;
+
+    correct = isCorrect();
+}
+
+void Date::setMonth(Month m) {
+    month = (unsigned)m;
+}
+
+bool Date::isCorrect() {
+    return true;
+}
--- Date.h
+++ Date.h
@@ -5,9 +5,36 @@
 #ifndef DATE_H
 #define DATE_H
 
+enum class Month {
+    JANUARY = 1,
+    FEBRUARY = 2,
+    MARCH = 3,
+    APRIL = 4,
+    MAI = 5,
+    JUNE = 6,
+    JULY = 7,
+    AUGUST = 8,
+    SEPTEMBER = 9,
+    OCTOBER = 10,
+    NOVEMBER = 11,
+    DECEMBER = 12
+};
 
 class Date {
+public:
+    Date() : day(1), month(1), year(1970) {};
+    Date(unsigned, unsigned, unsigned);
+    Date(unsigned, Month, unsigned);
 
+    void setMonth(Month m);
+
+private:
+    unsigned day;
+    unsigned month;
+    unsigned year;
+    bool correct;
+
+    bool isCorrect();
 };
 
 
